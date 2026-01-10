9a7628a09a786e47eca6e76171fc9d1b8f2d0e70
smihail-a <mihail.sofia.andreea@gmail.com>
2026-01-10 23:00:04 +0200

    test for fe

---
+++ stayeasy-springangular/src/app/pages/review/review.ts
@@ -0,0 +1,20 @@
+import {Component} from '@angular/core';
+import { NgForOf, NgIf } from '@angular/common';
+import { finalize } from 'rxjs/operators';
+import { LoginService } from '../../service/login-service';
+import { AuthService } from '../../service/auth-service';
+import { PropertyResponseDTO } from '../../models/property.models';
+import {PropertyService} from '../../service/property-service';
+import {CreatePropertyModal} from '../create-property-modal/create-property-modal';
+import {FormsModule} from '@angular/forms';
+
+ @Component({
+   selector: 'property-review',
+   imports: [NgIf, NgForOf, CreatePropertyModal, FormsModule],
+   templateUrl: './review.html',
+   styleUrls: ['./review.css'],
+   standalone: true
+ })
+export class ReviewPage {
+
+ }
