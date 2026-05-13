381dd1a8afe1e4f2bbda4987f15afda7770fe6c6
RaduAlex18 <116818618+RaduAlex18@users.noreply.github.com>
2026-05-13 15:24:33 +0300

    Test database configuration - database H2

--- stayeasy-springangular/src/test/java/com/stayeasy/stayeasyspringangular/Integration/PropertyIntegrationTest.java
+++ stayeasy-springangular/src/test/java/com/stayeasy/stayeasyspringangular/Integration/PropertyIntegrationTest.java
@@ -19,6 +19,7 @@ import org.springframework.http.MediaType;
 
 import org.springframework.security.test.context.support.WithMockUser;
 
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import org.springframework.transaction.annotation.Transactional;
@@ -35,6 +36,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
+@ActiveProfiles("test")
 public class PropertyIntegrationTest {
 
   @Autowired
